4ec217b6cf2a0387e89ea288c6cdfd395acc51a8
SetrigeCrawford <setrige.crawford@journalism.cuny.edu>
2025-03-26 02:21:05 -0400

    I added workout routine model

---
+++ server/models/WorkoutRoutine.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+
+const workoutRoutineSchema = new Schema (
+    {
+     user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+     },
+     excercises: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Excercise'
+     }],
+     duration: {
+        type: Number,
+        required: true
+     },
+     intensity: {
+        type: String, 
+        enum: ['Low', 'Medium', 'High'],
+        required: true
+     },
+     createdAt: {
+        type: Date,
+        default: Date.now
+     }
+
+
+    });
+
+    const WorkoutRoutine = mongoose.model('WorkoutRoutine', workoutRoutineSchema);
+
+
+
+
+module.exports = WorkoutRoutine;
