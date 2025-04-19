0302273d939e8047fd328a9ee16f0306cc59e3b4
SetrigeCrawford <setrige.crawford@journalism.cuny.edu>
2025-04-18 23:38:16 -0400

    I added some styles for my sign-in/sign-up page

--- client/src/components/Component.jsx
+++ client/src/components/Component.jsx
@@ -1,4 +1,31 @@
 import styled from 'styled-component';
 
 
-export const Container = styled.div 
+export const Container = styled.div`
+    background-color: #fff;
+    border-radius: 10px;
+    box-shadow: 0 14px 28px rgba(0, 0, 0.25), 0 10px 10px rgba(0, 0, 0, 0.22);
+    position: relative;
+    overflow: hidden;
+    width: 678px;
+    max-width: 100%;
+    min-height: 400px;
+`;
+
+export const SignUpContainer = styled.div`
+    position: absolute;
+    top: 0;
+    height: 100%;
+    transition: all 0.6s ease-in-out;
+    left: 0;
+    width: 50%;
+    opacity: 0;
+    z-index: 1;
+    ${props => props.signinIn !== true ?`
+        transform: translateX(100%);
+        opacity: 1;
+        z-index: 5;
+        `
+        : null}
+    };
+`;
