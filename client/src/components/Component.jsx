11f9a2f2299c6e79fd6f4eaa3e0390d7a9081caf
SetrigeCrawford <setrige.crawford@journalism.cuny.edu>
2025-05-27 17:47:25 -0400

    I added Home, User, Workout and Login/SignUp pages

--- client/src/components/Component.jsx
+++ client/src/components/Component.jsx
@@ -1,31 +1,59 @@
-import styled from 'styled-component';
+// import styled from 'styled-component';
 
 
-export const Container = styled.div`
-    background-color: #fff;
-    border-radius: 10px;
-    box-shadow: 0 14px 28px rgba(0, 0, 0.25), 0 10px 10px rgba(0, 0, 0, 0.22);
-    position: relative;
-    overflow: hidden;
-    width: 678px;
-    max-width: 100%;
-    min-height: 400px;
-`;
+// export const Container = styled.div`
+//     background-color: #fff;
+//     border-radius: 10px;
+//     box-shadow: 0 14px 28px rgba(0, 0, 0.25), 0 10px 10px rgba(0, 0, 0, 0.22);
+//     position: relative;
+//     overflow: hidden;
+//     width: 678px;
+//     max-width: 100%;
+//     min-height: 400px;
+// `;
 
-export const SignUpContainer = styled.div`
-    position: absolute;
-    top: 0;
-    height: 100%;
-    transition: all 0.6s ease-in-out;
-    left: 0;
-    width: 50%;
-    opacity: 0;
-    z-index: 1;
-    ${props => props.signinIn !== true ?`
-        transform: translateX(100%);
-        opacity: 1;
-        z-index: 5;
-        `
-        : null}
-    };
-`;
+// export const SignUpContainer = styled.div`
+//     position: absolute;
+//     top: 0;
+//     height: 100%;
+//     transition: all 0.6s ease-in-out;
+//     left: 0;
+//     width: 50%;
+//     opacity: 0;
+//     z-index: 1;
+//     ${props => props.signinIn !== true ?`
+//         transform: translateX(100%);
+//         opacity: 1;
+//         z-index: 5;
+//         `
+//         : null}
+//     };
+// `;
+
+// export const SignInContainer = styled.div`
+//     positiom: absolute;
+//     top: 0;
+//     height: 100%;
+//     export const SignUpContainer = styled.div`
+//     position: absolute;
+//     top: 0;
+//     height: 100%;
+//     transition: all 0.6s ease-in-out;
+//     left: 0;
+//     width: 50%;
+//     opacity: 0;
+//     z-index: 2;
+//     ${props => {props.signinIn !== true ?
+//         `transform: translateX(100%);`: null}}
+// `;
+
+// export const Form = styled.form`
+//         background: #fffff;
+//         display: flex;
+//         align-items: center;
+//         justify-content: center;
+//         flex-direction: column;
+//         padding
+
+
+// `
