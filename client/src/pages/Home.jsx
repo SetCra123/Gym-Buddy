4cf4a84c8f52c3826689235fc5badb1102362346
SetrigeCrawford <setrige.crawford@journalism.cuny.edu>
2025-05-29 14:16:45 -0400

    I added pages to front end, added code to Home.jsx

---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,18 @@
+import { useState, useEffect } from 'react';
+import './home.css';
+import { Link, useNavigate } from 'react-router-dom';
+import { getAllExcercises } from '../utils/API';
+
+const Home = () => {
+
+
+return (
+    <div className="home-container">
+        <h1>Welcome to Your Workout</h1>
+        
+    </div>
+)
+
+};
+
+export default Home;
