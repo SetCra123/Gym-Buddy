d6dd33c998e55e07d9b8c7717eb266f35cb4a1bf
SetrigeCrawford <setrige.crawford@journalism.cuny.edu>
2025-11-23 23:16:20 -0500

    README update, fix bugs

--- server/routes/api/userRoutes.js
+++ server/routes/api/userRoutes.js
@@ -14,13 +14,13 @@ const {
 // import middleware
 const { authMiddleware } = require('../../utils/auth');
 
-// put authMiddleware anywhere we need to send a token for verification of user
-router.route('/').post(createUser);
 
+router.route('/').post(createUser);
 router.route('/login').post(login);
 router.route('/').get(getUsers);
 router.route('/:userId').delete(removeUser);
 
+// put authMiddleware anywhere we need to send a token for verification of user
 router.route('/me').get(authMiddleware, getSingleUser);
 router.route('/me').put(authMiddleware, updateUserProfile);
 router.route('/assign-routine').put(authMiddleware, assignWorkoutRoutine);
